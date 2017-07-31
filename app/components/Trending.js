489d68af578dade3918e50ccbe2270448a74934c
Morgan Pierson <morganpierson@gmail.com>
2017-07-31 12:07:39 -0700

    trending component added state

--- app/components/Trending.js
+++ app/components/Trending.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 
 class Trending extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedGenre: "All"
+    };
+
+    this.updateGenre = this.updateGenre.bind(this);
+  }
+
+  updateGenre(genre) {
+    this.setState({ selectedGenre: genre });
+  }
+
   render() {
     const genres = ["All", "Rock", "Hip-Hop", "R&B", "EDM"];
 
@@ -8,7 +22,13 @@ class Trending extends Component {
       <ul className="genres">
         {genres.map(genre => {
           return (
-            <li>
+            <li
+              key={genre}
+              style={
+                genre === this.state.selectedGenre ? { color: "#d0021b" } : null
+              }
+              onClick={this.updateGenre.bind(null, genre)}
+            >
               {genre}
             </li>
           );
